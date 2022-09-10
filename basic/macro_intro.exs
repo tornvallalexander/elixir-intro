b96c2642fc8133c7387e8c3ff41bc8ed14be51ac
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-09-10 16:32:22 +0200

    feat: using quote and unquote macro

--- basic/macro_intro.exs
+++ basic/macro_intro.exs
@@ -2,4 +2,11 @@ defmodule Util do
   defmacro reverse({:+, context, [a, b]}) do
     {:-, context, [a, b]}
   end
+
+  # reversing with quote and unquote macro (automatically created te 'Abstract Syntax Tree' for us)
+  defmacro reverse2({:+, context, [a, b]}) do
+    quote do
+      unquote(a) - unquote(b)
+    end
+  end
 end
