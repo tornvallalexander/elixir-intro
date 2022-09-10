73e02eea0e3b838f4375564eeaabb34054f24ca9
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-09-10 10:56:10 +0200

    feat: composition with stream module instead of enum for better perf

---
+++ basic/stream.exs
@@ -0,0 +1,17 @@
+defmodule Composition do
+  import Integer, only: [is_even: 1]
+
+  def eager(nums) do
+    nums
+    |> Enum.map(&(&1 * 2))
+    |> Enum.filter(&is_even/1)
+    |> Enum.sum()
+  end
+
+  def lazy(nums) do
+    nums
+    |> Stream.map(&(&1 * 2))
+    |> Stream.filter(&is_even/1)
+    |> Enum.sum()
+  end
+end
